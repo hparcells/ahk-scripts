1c90947f22f2b5d50d10997ea0394c392e530571
Hunter Parcells <hparcells@icloud.com>
2020-12-06 17:30:31 -0500

    Better Home Depot

--- js/home-depot.js
+++ js/home-depot.js
@@ -1,13 +1,17 @@
 const fetch = require('node-fetch');
 
+const randomNumber = () => {
+  return ['20', '21', '30', '31'][Math.floor((Math.random()*4))] + ((Math.random()*1e7).toFixed(0).padStart(7, '0')).toString()
+}
+
 const checkUrl = () => {
-    let url = 'https://www.homedepot.com/p/20'+(Math.random() * 1e7).toFixed(0).padStart(7, '0');
-    fetch(url, {}).then(res => {
-        if (res.status >= 300) {
-            return checkUrl();
-        }
-        console.log(url);
-    });
+  let url = 'https://www.homedepot.com/p/'+randomNumber();
+  fetch(url, {}).then(res => {
+      if (res.status >= 300) {
+        return checkUrl();
+      }
+      console.log(url);
+  });
 };
 
 checkUrl();
